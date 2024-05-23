24c658daead638d56653280b5328ae99fe1a5ae2
setthi-pheap <157299176+setthi-pheap@users.noreply.github.com>
2024-05-23 16:21:12 +0700

    new

---
+++ app/Http/Controllers/Admin/ScholarshipCrudController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ScholarshipRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ScholarshipCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class ScholarshipCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Scholarship::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/scholarship');
+        CRUD::setEntityNameStrings('scholarship', 'scholarship');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::setFromDb(); // set columns from db columns.
+
+        /**
+         * Columns can be defined using the fluent syntax:
+         * - CRUD::column('price')->type('number');
+         */
+        CRUD::addColumn([
+            'name' => 'name',
+            'label' => 'Name',
+            'type' => 'text',
+        ]);
+        CRUD::addColumn([
+            'name' => 'description',
+            'label' => 'Description',
+            'type' => 'textarea',
+        ]);
+        // CRUD::addColumn([
+        //     'name' => 'image',
+        //     'label' => 'image',
+        //     'type' => 'image',
+        // ]);
+       
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(ScholarshipRequest::class);
+        CRUD::setFromDb(); // set fields from db columns.
+
+        /**
+         * Fields can be defined using the fluent syntax:
+         * - CRUD::field('price')->type('number');
+         */
+        CRUD::addField([
+            'name' => 'name',
+            'label' => 'Name',
+            'type' => 'text',
+        ]);
+        CRUD::addField([
+            'name' => 'description',
+            'label' => 'Description',
+            'type' => 'textarea',
+        ]);
+        // CRUD::addField([
+        //     'name' => 'image',
+        //     'label' => 'image',
+        //     'type' => 'image',
+        // ]);
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
